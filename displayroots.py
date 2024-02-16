e9718d87856ebda3cec5ec395428de3328f45232
Michal Leszczynski <ml@icedev.pl>
2024-02-16 23:11:08 +0100

    add displayroots tool, minor fixes in validatecli

---
+++ displayroots.py
@@ -0,0 +1,6 @@
+from pyhanko.keys import load_certs_from_pemder
+
+root_certs = list(load_certs_from_pemder(['trust_roots.pem']))
+
+for root_cert in root_certs:
+    print(root_cert.subject.human_friendly)
--- validatecli.py
+++ validatecli.py
@@ -17,14 +17,17 @@ root_certs = list(load_certs_from_pemder(['trust_roots.pem']))
 
 with open(args.input_file, 'rb') as f:
     data = io.BytesIO(f.read())
-    print('Title: ' + get_title_from_io(data))
+    print('Document title: ' + get_title_from_io(data))
 
     data.seek(0)
     r = PdfFileReader(data)
 
     for sig in r.embedded_signatures:
         cert_subj = sig.signer_cert[0]["subject"]
-        signer_text = cert_subj.native.get('serial_number', '<None>') + ' ' + cert_subj.native.get('common_name', '<None>')
+        serial_no = cert_subj.native.get('serial_number', '<None>')
+        common_name = cert_subj.native.get('common_name', '<None>')
+
+        signer_text = f'[{serial_no}] {common_name}'
 
         try:
             sig_status = validate_pdf_ltv_signature(
@@ -33,10 +36,11 @@ with open(args.input_file, 'rb') as f:
                 validation_context_kwargs={'trust_roots': root_certs}
             )
         except ValueErrorWithMessage as e:
-            print('FAIL ' + signer_text + ' ' + str(e))
+            print(f'FAIL - {signer_text} - {e}')
             continue
 
         if not sig_status.bottom_line:
             raise RuntimeError('Failed to validate signature for: ' + signer_text)
 
-        print('OK ' + signer_text + ' ' + sig_status.timestamp_validity.timestamp.isoformat())
+        ts = sig_status.timestamp_validity.timestamp.isoformat()
+        print(f'OK - {signer_text} - {ts}')
